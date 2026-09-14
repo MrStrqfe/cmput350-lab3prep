798750d7ca2753b40df9bef6fb3545bc6b0a3900
Jensen Lee <jensen5@ualberta.ca>
2026-09-14 16:44:27 -0600

    Implemented lab3 prep

--- src/UniquePtr.h
+++ src/UniquePtr.h
@@ -1,6 +1,103 @@
 #ifndef UNIQUE_PTR_H
 #define UNIQUE_PTR_H
 
+#include <utility>
+
 // Your implementation here
+template<class T>
+class UniquePtr {
+public:
+    // Constructors
+    UniquePtr()
+        : mPtr(nullptr) {}    
+
+    UniquePtr(T* p) : mPtr(p) {}
+
+    ~UniquePtr<T>() {
+        if (mPtr != nullptr) {
+            delete mPtr;
+        }    
+    }
+
+    // Disable the copy semantics
+    // Copy Constructor
+    UniquePtr(const UniquePtr&) = delete;
+    
+    // Copy Assignment Operator
+    UniquePtr& operator = (const UniquePtr&) = delete;
+
+
+    // Source: https://uofa-cmput350.github.io/materials/lab/3/cpp.html#example-of-a-class-with-move-constructor-assignment
+    // Move constructor
+    UniquePtr(UniquePtr&& other) : mPtr{other.mPtr}
+    {
+        other.mPtr = nullptr;
+    }
+
+    // Move Assignment
+    UniquePtr& operator=(UniquePtr&& other) noexcept {
+        if (this != &other) {
+            delete mPtr;
+            mPtr = other.mPtr;
+            other.mPtr = nullptr;
+        }
+        return *this;
+    }
+    
+    template<class U>
+    UniquePtr(UniquePtr<U>&& other): mPtr{other.release()} {}
+
+    // Deference
+    T& operator*() const {
+        return *mPtr;
+    }
+
+    // Arrow
+    T* operator->() const {
+        return mPtr;
+    }
+
+    // get()
+    T* get() const {
+        return mPtr;
+    }
+
+    // Equality
+    bool operator==(const UniquePtr<T>& other) const {
+        return mPtr == other.mPtr;
+    }
+
+    // Nonempty check
+    explicit operator bool() const {
+        return mPtr != nullptr;
+    }
+
+    // Mutators
+    T* release() {
+        return std::exchange(mPtr, nullptr);
+    }
+
+    void reset(T* newPtr = nullptr) {
+        T* old = std::exchange(mPtr, newPtr);
+        delete old;
+    }
+
+    void swap(UniquePtr<T>& other) {
+        using std::swap;
+        swap(mPtr, other.mPtr);
+    }
+
+private:
+    T* mPtr;
+    template<class U>
+    friend class UniquePtr;
+
+};
+
+// makeUnique template function
+template<typename T, typename... Args>
+    UniquePtr<T> makeUnique(Args&&... args) {
+        return UniquePtr<T>(new T(std::forward<Args>(args)...));
+    }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include "UniquePtr.h"
+#include <iostream>
+
+// created a simple Point class to test
+struct Point {
+    int x,y;
+    Point(int x, int y): x(x), y(y) {}
+};
 
 int main() {
     // UniquePtr<int> ptr(new int(5));
     // they test all methods here...
+    UniquePtr<Point> p (new Point(3,4));
+    std::cout << "p = (" << p->x << ", " << p->y << ")\n";
+    
+    // Move constructor test
+    UniquePtr<Point> p2 = std::move(p);
+    std::cout << "p is now empty: " << (!p) << "\n";
+    std::cout << "p2 = (" << p2->x << ", " << p2->y << ")\n";
+
+    //release / reset
+    p2.reset(new Point(1,2));
+    std::cout << "p2 after reset = (" << p2->x << ", " << p2->y << ")\n";
+
+    // makeUnique
+    UniquePtr<Point> p3 = makeUnique<Point>(7,8);
+    std::cout << "p3 = (" << p3->x << ", " << p3->y << ")\n";
+
     return 0;
 }
